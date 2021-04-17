c09c1050f3374530749dfac341b2aeb61eca2b79
Noor ahmad <NourAhmedAbdulSattar41@gmail.com>
2021-04-16 19:07:59 -0700

    write file gradel using kotlin

---
+++ buildSrc/src/main/java/Deps.kt
@@ -0,0 +1,15 @@
+object Deps {
+    val kotlin_sblib = "org.jetbrains.kotlin:kotlin-stdlib:${Version.kotlin_version}"
+    val core_ktx = "androidx.core:core-ktx:${Version.core_ktx}"
+    val appcompat = "androidx.appcompat:appcompat:${Version.appcompat}"
+    val material = "com.google.android.material:material:${Version.material}"
+    val constraintlayout = "androidx.constraintlayout:constraintlayout:${Version.constraintlayout}"
+    val junit = "junit:junit:${Version.junit}"
+    val test_ext = "androidx.test.ext:junit:${Version.test_ext}"
+    val espresso_core = "androidx.test.espresso:espresso-core:${Version.espresso_core}"
+
+    val applicationId = "com.ibareq.the_chance_week6"
+    val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+
+}
---
+++ buildSrc/src/main/java/Version.kt
@@ -0,0 +1,20 @@
+object Version {
+
+    val compileSdkVersion = 30
+    val buildToolsVersion = "30.0.1"
+    val minSdkVersion = 23
+    val targetSdkVersion = 30
+    val versionCode = 1
+    val versionName = "1.0"
+    val kotlin_version = "1.3.72"
+    val core_ktx = "1.3.2"
+    val appcompat = "1.2.0"
+    val material = "1.3.0"
+    val constraintlayout = "2.0.4"
+    val junit = "4.13.2"
+    val test_ext = "1.1.2"
+    val espresso_core = "3.3.0"
+    val jvmTarget = "1.8"
+
+
+}
